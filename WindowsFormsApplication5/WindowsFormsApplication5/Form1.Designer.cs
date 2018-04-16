eb89b160e761fa69a6a45c59ead7541a95af8755
BOoll18 <35131621+BOoll18@users.noreply.github.com>
2018-04-16 21:21:55 +0300

    Add files via upload

---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonstop = new System.Windows.Forms.Button();
+            this.buttonunpause = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(77, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(1033, 431);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 132);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.DimGray;
+            this.pictureBox2.Image = global::WindowsFormsApplication1.Properties.Resources._0c3d3a1e1403b36c2120361ea93e03a799a96035;
+            this.pictureBox2.Location = new System.Drawing.Point(52, 512);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(171, 62);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(41, 309);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1117, 296);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.Red;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(494, 171);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(308, 84);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Visible = false;
+            // 
+            // buttonstop
+            // 
+            this.buttonstop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonstop.Location = new System.Drawing.Point(997, 31);
+            this.buttonstop.Name = "buttonstop";
+            this.buttonstop.Size = new System.Drawing.Size(75, 29);
+            this.buttonstop.TabIndex = 6;
+            this.buttonstop.Text = "PAUSE";
+            this.buttonstop.UseVisualStyleBackColor = true;
+            this.buttonstop.Visible = false;
+            this.buttonstop.Click += new System.EventHandler(this.buttonstop_Click_1);
+            // 
+            // buttonunpause
+            // 
+            this.buttonunpause.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonunpause.Location = new System.Drawing.Point(997, 80);
+            this.buttonunpause.Name = "buttonunpause";
+            this.buttonunpause.Size = new System.Drawing.Size(117, 30);
+            this.buttonunpause.TabIndex = 7;
+            this.buttonunpause.Text = "Unpause";
+            this.buttonunpause.UseVisualStyleBackColor = true;
+            this.buttonunpause.Visible = false;
+            this.buttonunpause.Click += new System.EventHandler(this.buttonunpause_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(766, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "0";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1314, 609);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonunpause);
+            this.Controls.Add(this.buttonstop);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonstop;
+        private System.Windows.Forms.Button buttonunpause;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsFormsApplication5/WindowsFormsApplication5/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            pictureBox2.Show();
+            pictureBox1.Show();
+            pictureBox3.Show();
+            textBox2.Visible = false;
+            buttonstop.Show();
+            button1.Visible = false;
+            
+
+        }
+
+        private void buttonstop_Click_1(object sender, EventArgs e)
+        {
+            buttonunpause.Enabled = true;
+            buttonunpause.Show();
+            pictureBox3.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox1.Visible = false;
+            textBox2.Visible = false;
+            buttonstop.Visible = false;
+            
+        }
+      private void buttonunpause_Click(object sender, EventArgs e)
+        {
+
+            buttonunpause.Enabled = false;
+            buttonunpause.Visible = false;
+            button1.Enabled = true;
+            pictureBox2.Show();
+            pictureBox1.Show();
+            pictureBox3.Show();
+            textBox2.Visible = false;
+            buttonstop.Show();
+            
+        }
+       private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+            {
+                pictureBox2.Left = pictureBox2.Left + 5;
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                pictureBox2.Left = pictureBox2.Left - 5;
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                pictureBox2.Top = pictureBox2.Top - 5;
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                pictureBox2.Top = pictureBox2.Top + 5;
+            }
+            
+        }
+
+
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            textBox2.Enabled = false;
+            if (pictureBox2.Bounds.IntersectsWith(pictureBox3.Bounds))
+            {
+                textBox2.Visible = true;
+                timer1.Enabled = false;
+                textBox2.Text = "YOU WON! Seconds until the automatical exit: ";
+                textBox2.Enabled = true;
+                buttonstop.Visible = false;
+              
+               
+                    
+               
+            }
+            
+            
+        }
+        int i = 5;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            i--;
+            label1.Text = i.ToString() + "Seconds";
+          
+        }
+      
+
+      
+        
+
+       
+
+        
+
+       
+
+        
+
+       
+
+        
+
+        
+
+        
+
+
+      
+
+        
+        
+
+     
+
+       
+    }
+}
